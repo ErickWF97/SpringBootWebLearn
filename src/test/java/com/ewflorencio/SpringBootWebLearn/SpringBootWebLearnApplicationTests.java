b2623fc1e3b25d007b64dca4efa5a40e8b756939
Erick W. Florencio <erick_w.florencio@outlook.com>
2024-11-17 16:15:01 -0300

    start project

---
+++ src/test/java/com/ewflorencio/SpringBootWebLearn/SpringBootWebLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ewflorencio.SpringBootWebLearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebLearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
